2998fef303bd5534fde8e179ca455b98c26f53c1
Antonio <atcarter005@gmail.com>
2021-03-06 16:16:06 -0600

    commit tree e20284a903eb3902d54d9085937d9827f2ad47f8

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        str = "#{self.first_name} #{self.last_name}"
+        str
+    end
+    
+    def list_roles
+        arr = []
+        characters.each do |c|
+            str = "#{c.name} - #{c.show.name}"
+            arr << str
+        end
+        arr
+    end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+
+    def actors_list
+        arr = []
+        self.characters.each do |c|
+            arr << c.actor.full_name
+        end
+        arr
+    end
 end
